b7b3f0e385edc883e60bd9fdcba21bd694614c79
Sourav Ghosh <sourav011990@gmail.com>
2026-05-19 21:31:34 +0530

    chore: add testing dependencies and configure Jest in frontend
    - Updated package.json to include Jest and testing libraries.
    - Added "test" script to package.json for running tests.
    - Modified tsconfig.json to include Jest types for TypeScript.

---
+++ frontend/jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "jest"
+import nextJest from "next/jest.js"
+
+const createJestConfig = nextJest({
+    dir: "./"
+})
+
+const customConfig: Config = {
+    testEnvironment: "jsdom",
+
+    setupFilesAfterEnv: [
+        "<rootDir>/jest.setup.ts"
+    ],
+
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/$1"
+    }
+}
+
+export default createJestConfig(customConfig)
